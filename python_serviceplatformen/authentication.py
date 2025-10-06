6ce377d9be77605ee71aefe10ab88465e2d38eac
Mathias G <ghbm@aarhus.dk>
2025-10-06 14:18:35 +0200

    Added Token dataclass

--- python_serviceplatformen/authentication.py
+++ python_serviceplatformen/authentication.py
@@ -3,6 +3,7 @@ Kombit API.
 """
 
 from datetime import datetime, timedelta
+from dataclasses import dataclass
 
 import requests
 from requests.exceptions import HTTPError
@@ -11,13 +12,19 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 
 
+@dataclass
+class Token:
+    value: str
+    expiry_time: datetime
+
+
 # pylint: disable-next=too-few-public-methods
 class KombitAccess:
     """An object that handles access to the Kombit api."""
     cvr: str
     cert_path: str
-    _access_tokens: dict[str, (str, datetime)]
-    _saml_tokens: dict[str, (str, datetime)]
+    _access_tokens: dict[str, Token]
+    _saml_tokens: dict[str, Token]
     test: bool
     environment: str
 
@@ -53,20 +60,20 @@ class KombitAccess:
         Raises:
             ValueError: If an access token couldn't be obtained for the given entity id.
         """
-        if entity_id in self._access_tokens and datetime.now() < self._access_tokens[entity_id][1]:
-            return self._access_tokens[entity_id][0]
+        if entity_id in self._access_tokens and datetime.now() < self._access_tokens[entity_id].expiry_time:
+            return self._access_tokens[entity_id].value
 
         try:
             saml_token = self.get_saml_token(entity_id)
             access_token = _get_access_token(saml_token, self.cert_path, test=self.test)
             self._access_tokens[entity_id] = access_token
-            return access_token[0]
+            return access_token.value
         except HTTPError as exc:
             raise ValueError(f"Couldn't obtain access token for {entity_id}: {exc.response.text}") from exc
 
     def get_saml_token(self, entity_id: str) -> str:
         """Get a SAML to the api endpoint with the given entity id.
-        If a SAML token already exists for the endpoint it is reused.
+        If a valid SAML token already exists for the endpoint it is reused.
 
         Args:
             entity_id: The entity id of the endpoint.
@@ -77,18 +84,18 @@ class KombitAccess:
         Returns:
             The SAML token as a string
         """
-        if entity_id in self._saml_tokens and datetime.now() < self._saml_tokens[entity_id][1]:
-            return self._saml_tokens[entity_id][0]
+        if entity_id in self._saml_tokens and datetime.now() < self._saml_tokens[entity_id].expiry_time:
+            return self._saml_tokens[entity_id].value
 
         try:
             saml_token = _get_saml_token(self.cvr, self.cert_path, entity_id, test=self.test)
             self._saml_tokens[entity_id] = saml_token
-            return saml_token[0]
+            return saml_token.value
         except HTTPError as exc:
             raise ValueError(f"Couldn't obtain SAML token for {entity_id}: {exc.response.text}") from exc
 
 
-def _get_saml_token(cvr: str, cert_path: str, entity_id: str, test: bool) -> tuple[str, datetime]:
+def _get_saml_token(cvr: str, cert_path: str, entity_id: str, test: bool) -> Token:
     """Get a SAML token for the endpoint with the given entity id.
 
     Args:
@@ -130,10 +137,10 @@ def _get_saml_token(cvr: str, cert_path: str, entity_id: str, test: bool) -> tup
     saml_token = response['RequestedSecurityToken']['Assertion']
     expiry_time = datetime.strptime(response["Lifetime"], "%m/%d/%Y %H:%M:%S") - timedelta(minutes=5)
 
-    return saml_token, expiry_time
+    return Token(saml_token, expiry_time)
 
 
-def _get_access_token(saml_token: str, cert_path: str, test: bool) -> tuple[str, datetime]:
+def _get_access_token(saml_token: str, cert_path: str, test: bool) -> Token:
     """Get an access token for the given SAML context.
 
     Args:
@@ -160,7 +167,7 @@ def _get_access_token(saml_token: str, cert_path: str, test: bool) -> tuple[str,
     access_token = f"{response['token_type']} {response['access_token']}"
     expiry_time = datetime.now() + timedelta(seconds=response['expires_in']) - timedelta(minutes=5)
 
-    return access_token, expiry_time
+    return Token(access_token, expiry_time)
 
 
 def _extract_first_certificate(pem_file: str) -> str:
